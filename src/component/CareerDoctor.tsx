7fd12744a2eaa0538bae8e34850bb9dda42027a1
M Waseem <muhammadwaseem2u@gmail.com>
2025-01-15 00:37:05 -0800

    design final

--- src/component/CareerDoctor.tsx
+++ src/component/CareerDoctor.tsx
@@ -7,6 +7,38 @@ const CareerDoctor = () => {
   return (
     <div className="px-2 lg:px-40 mt-4">
       <div className="vertical-bar pl-4 lg:pl-16 top-4">
+        <div className="relative">
+          <div className="absolute -left-6 top-[4px] lg:-left-20  lg:top-[1px] w-4 h-4 lg:w-[32px] lg:h-[32px] stars-shadow border border-light-violet rounded-full"></div>
+          <div className="">
+            <div className=" w-full max-w-[1132px] flex justify-between items-center border-b border-natural-grey pb-6">
+              <div className=" flex justify-center items-center gap-2 lg:gap-14">
+                <div className="flex justify-center items-center gap-2 ">
+                  <div className="w-6 h-6 lg:w-[30px] lg:h-[30px] border border-purple-heart rounded-full flex justify-center items-center">
+                    <CareerPlay />
+                  </div>
+
+                  <span className="text-base lg:text-xl font-space-gori font-normal text-nile-blue">
+                    1.
+                  </span>
+                  <p className="text-base lg:text-xl font-space-gori font-normal text-nile-blue">
+                    Introduction of Doctor Career
+                  </p>
+                </div>
+                <div className="flex justify-center items-center rounded-lg bg-off-green">
+                  <p className="text-xs lg:text-base font-semibold text-caribbean-green px-1 py-2 lg:px-4 lg:py-[10px]">
+                    10% Completed
+                  </p>
+                </div>
+              </div>
+              <div>
+                <span className="text-xs lg:text-base font-space-gori font-semibold text-blue-grey">
+                  10 minutes
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {CAREER_D0CTOR.map((x, index) => (
           <div key={index} className="relative">
             <div className="absolute -left-6 lg:-left-20 top-8 lg:top-7 w-4 h-4 lg:w-[32px] lg:h-[32px] stars-shadow border border-light-violet rounded-full"></div>
@@ -175,21 +207,13 @@ export const CAREER_D0CTOR = [
   {
     id: 1,
     image: <CareerPlay />,
-    count: "1.",
-    name: "Introduction of Doctor Career",
-    percent: " 10% Completed",
-    minute: "10 minutes",
-  },
-  {
-    id: 2,
-    image: <CareerPlay />,
     count: "2.",
     name: "Introduction of Doctor Career",
     percent: " 20% Completed",
     minute: "10 minutes",
   },
   {
-    id: 3,
+    id: 2,
     image: <CareerPlay />,
     count: "3.",
     name: "Introduction of Doctor Career",
@@ -197,7 +221,7 @@ export const CAREER_D0CTOR = [
     minute: "10 minutes",
   },
   {
-    id: 4,
+    id: 3,
     image: <CareerPlay />,
     count: "4.",
     name: "Introduction of Doctor Career",
@@ -205,7 +229,7 @@ export const CAREER_D0CTOR = [
     minute: "10 minutes",
   },
   {
-    id: 5,
+    id: 4,
     image: <CareerPlay />,
     count: "5.",
     name: "Introduction of Doctor Career",
